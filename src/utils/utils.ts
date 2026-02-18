0b70a9e771f4b96d4d263d2101a15e823da58584
Esteban Huerta <esteban.huerta@eversource.com>
2026-02-18 16:11:14 -0300

    check window

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const isProduction = window.location.hostname === 'metroprop.co';
+export const isProduction = window?.location.hostname === 'metroprop.co';
 
 export const API_BASE_URL = isProduction
   ? 'https://api.metroprop.co/'
