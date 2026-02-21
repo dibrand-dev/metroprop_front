7abca2af13463d04948d855634d0429392ccbf77
emilio huerta <e.huerta@newsweek.com>
2026-02-21 15:52:57 -0300

    hostname prod

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const isProduction = typeof window !== 'undefined' && window.location.hostname === 'metroprop.co';
+export const isProduction = typeof window !== 'undefined' && window.location.hostname === 'www.metroprop.co';
 
 export const API_BASE_URL = isProduction
   ? 'https://api.metroprop.co/'
