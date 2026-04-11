dc6a0fd0a5192276482321bcca7867f8647d34ea
eshuerta <eshuerta@gmail.com>
2026-04-10 22:41:55 -0300

    suggestions priorizaciones

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { API_BASE_URL } from '@/utils/utils';
+import { LOCATION_ARGENTINA_ID } from '@/app/constants';
 
 export interface Location {
   id: number;
@@ -21,7 +22,7 @@ export const LOCATIONS_QUERY_KEY = ['locations-global'];
  * Fetch all locations from the API
  */
 export const fetchLocations = async (): Promise<Location[]> => {
-  const response = await fetch(`${API_BASE_URL}/location/getAllLocations?country_id=1`);
+  const response = await fetch(`${API_BASE_URL}/location/getAllLocations?country_id=${LOCATION_ARGENTINA_ID}`);
   if (!response.ok) throw new Error('Error fetching locations');
   return response.json();
 };
