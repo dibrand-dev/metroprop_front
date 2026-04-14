edc8dd0de1c149055a2a30939da3d4b1ea048963
eshuerta <eshuerta@gmail.com>
2026-04-13 22:02:18 -0300

    sin barra domian s3

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,4 +1,4 @@
-export const AWS_S3_BUCKET_URL = 'https://s3.us-east-2.amazonaws.com/www.metroprop.co/';
+export const AWS_S3_BUCKET_URL = 'https://s3.us-east-2.amazonaws.com/www.metroprop.co';
 
 export const LOCATION_ARGENTINA_ID = 1; 
 export const LOCATION_CABA_ID = 146;
